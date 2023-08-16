6a897a2c02be0182e0d83445bdb901676622085f
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-08-16 15:40:49 +0900

    add pods

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import KakaoSDKCommon
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      KakaoSDK.initSDK(appKey: "${NATIVE_APP_KEY}")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
